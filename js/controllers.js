c208881ce03860770e34d559e31f3d968495dd56
Kevin Shreve <kshreve@gmail.com>
2014-06-21 19:36:27 -0400

    work on email

--- js/controllers.js
+++ js/controllers.js
@@ -16,12 +16,26 @@ function GourdArtSoldCtrl($scope, $cookies, $location, $http) {
 }
 
 function ContactCtrl($scope, $http) {
+    var data = {
+        from: $scope.emailAddress,
+        to: 'kshreve@gmail.com',
+        subject: $scope.fname+' '+$scope.lname + ' Designs By Reetsie Inquiry ' + Date.now(),
+        text: $scope.message + '<br/>Phone number to contact: ' + $scope.phone
+    };
+               
+
 	$scope.submit = function() {
-	    var mailOptions = {
-	        from: $scope.emailAddress,
-	        to: 'kshreve@gmail.com',
-	        subject: $scope.fname+' '+$scope.lname + 'Designs By Reetsie Inquiry' + Date.now(),
-	        text: $scope.message + '<br/>Phone number to contact' + $scope.phone
-	    }
+        $.ajax({
+            type: "POST",
+            url: "/sendMail",
+            data: data,
+            success: function() {
+                console.log('mail sent');
+            },
+            error: function() {
+                console.log('something went wrong');
+            }
+        });
+	    
     };
 }
--- server.js
+++ server.js
@@ -1,21 +1,36 @@
 var express = require('express');
 var app = express();
-var nodemailer = require("nodemailer");
+var mail = require("nodemailer");
 
 app.use(app.router);
 app.use(express.static(__dirname));
 
 app.post('/sendMail',function(req, res){
     res.type('text/plain');
-    var mailOptions = {
-	        from: '',
-	        to: 'kshreve@gmail.com',
-	        subject: Date.now(),
-	        text: '<br/>Phone number to contact'
-	};
-
-    var transport = nodemailer.createTransport("Sendmail");
-    transport.sendMail(mailOptions);
+    
+    var smtpTransport = nodemailer.createTransport("SMTP",{
+       service: "Gmail",
+       auth: {
+           XOAuth2: {
+            user: "436379670934-j9f264cjcaucr7mgnfg2p9fkdpib2s3n@developer.gserviceaccount.com",
+            clientId: "436379670934-j9f264cjcaucr7mgnfg2p9fkdpib2s3n.apps.googleusercontent.com",
+            clientSecret: "_-TRbNKuCFmtE0uLkquq0EoC"
+        }
+       }
+    });
+    
+    smtpTransport.sendMail({
+        from: 'shreveke@gmail.com',
+	    to: 'kshreve@gmail.com',
+	    subject: Date.now(),
+	    text: '<br/>Phone number to contact'
+	}, function (error,response){
+        if(error) {
+            console.log(error);
+        } else {
+            console.log("message sent:" + response.message);
+        }
+    });
 });
 
 app.listen(process.env.PORT || 3000);
