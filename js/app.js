696f486b81f5fcf85436af90aa386cb20bc2d1ff
KShreve <kshreve@gmail.com>
2015-01-16 15:45:40 +0000

    changing from silly variable naming

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 angular.module('DesignsByReetsie', ['ngCookies','DesignsByReetsieFilters']).
-    config(['$routeProvider', function($routeProvider) {
-        $routeProvider
+    config(['$routeProvider', function(routeProvider) {
+        routeProvider
             .when('/', {templateUrl: 'partials/home.html', controller: HomeController})
             .when('/About',{templateUrl: 'partials/about.html', controller: AboutController})
             .when('/Awards', {templateUrl:'partials/awards.html', controller: AwardController})
--- js/controllers.js
+++ js/controllers.js
@@ -1,37 +1,37 @@
-function HomeController($scope, $cookies, $location, $http) {
+function HomeController(scope, cookies, location, http) {
 
 }
 
-function AboutController($scope, $cookies, $location, $http) {
+function AboutController(scope, cookies, location, http) {
 
 }
 
 
-function AwardController($scope, $cookies, $location, $http) {
+function AwardController(scope, cookies, location, http) {
 
 }
 
-function GourdArtSoldController($scope, $cookies, $location, $http) {
+function GourdArtSoldController(scope, cookies, location, http) {
 
 }
 
-function ContactController($scope, $http) {
-    $scope.submit = function() {
+function ContactController(scope, http) {
+    scope.submit = function() {
         var messageData = {
-            from: $scope.emailAddress,
-            name: $scope.fname + ' ' + $scope.lname,
-            text: $scope.message,
-            phone: $scope.phone
+            from: scope.emailAddress,
+            name: scope.fname + ' ' + scope.lname,
+            text: scope.message,
+            phone: scope.phone
         };
 
-        $http.post("/sendMail", messageData)
+        http.post("/sendMail", messageData)
              .success(function(data, status, headers, config) {
                 //console.log('success', data, status);
-                $scope.emailAddress='';
-                $scope.fname='';
-                $scope.lname='';
-                $scope.message='';
-                $scope.phone='';
+                scope.emailAddress='';
+                scope.fname='';
+                scope.lname='';
+                scope.message='';
+                scope.phone='';
              })
              .error(function(data, status, headers, config) {
                 console.log('error', data, status);
