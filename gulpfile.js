d91606429e6d72dc349d39de6788697ab81b05fa
Kevin Shreve <kshreve@gmail.com>
2015-01-23 14:59:24 -0500

    gulp-task working css now.
    Minification still breaking angularjs though.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
-var minifycss = require('gulp-minify-css');
+var minifyCSS = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var notify = require('gulp-notify');
 var minifyHTML = require('gulp-minify-html');
@@ -9,24 +9,30 @@ var del = require('del');
 var livereload = require('gulp-livereload');
 
 gulp.task('move', function() {
-    var filesToMove = ['index.html', 'server.js', 'Procfile', 'img/*.*', 'node_modules/**/*.*', 'lib/angular/angular.min.js', 'lib/**/*.*'];
-    return gulp.src(filesToMove, {base: './'})
+    return gulp.src(['index.html', 'server.js', 'Procfile', 'img/*.*', 'node_modules/**/*.*', 'lib/angular/angular.min.js', 'lib/**/*.*'], {base: './'})
                .pipe(gulp.dest('dist'));
 });
 
 gulp.task('scripts', function() {
   return gulp.src('js/*.js')
              .pipe(concat('app.min.js'))
-             .pipe(uglify())
+             /*.pipe(uglify())*/
              .pipe(gulp.dest('dist/js'))
              .pipe(livereload())
-             .pipe(notify({ message: 'Scripts task complete' }));
+             .pipe(notify({ message: 'Scripts task complete'}));
 });
 
-gulp.task('css', function() {
-    return gulp.src('css/*.css')
+gulp.task('css-lib', function() {
+   return gulp.src(['css/zimit.min.css'], {base: './'})
+              .pipe(rename('zimit.min.css'))
+              .pipe(gulp.dest('dist/css/'))
+              .pipe(notify({message: 'css-library task complete'}));
+})
+
+gulp.task('css', ['css-lib'], function() {
+    return gulp.src('css/app.css')
                .pipe(rename('app.min.css'))
-               .pipe(minifycss())
+               /*.pipe(minifyCSS())*/
                .pipe(gulp.dest('dist/css/'))
                .pipe(livereload())
                .pipe(notify({message: 'CSS task complete'}));
@@ -35,7 +41,7 @@ gulp.task('css', function() {
 gulp.task('html', function() {
     return gulp.src('partials/*.html')
                .pipe(rename('partials.html'))
-               .pipe(minifyHTML())
+               /*.pipe(minifyHTML())*/
                .pipe(gulp.dest('dist/partials'))
                .pipe(livereload())
                .pipe(notify({message: 'html-partial task complete'}));
@@ -48,8 +54,7 @@ gulp.task('clean', function(){
 gulp.task('default', ['move', 'scripts', 'css', 'html']);
 
 gulp.task('watch', function() {
+    var filesToWatch = ['index.html', 'server.js', 'Procfile', 'img/*.*', 'node_modules/**/*.*', 'lib/angular/angular.min.js', 'lib/**/*.*', 'js/*.js', 'css/*.css', 'partials/*.html'];
     livereload.listen();
-    gulp.watch('js/*.js', ['scripts']);
-    gulp.watch('css/*.css', ['css']);
-    gulp.watch('partials/*.html', ['html']);
+    gulp.watch(filesToWatch, ['default']);
 });
