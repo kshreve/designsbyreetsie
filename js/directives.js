6201766676513c2524cd9fd262698a46f57f77f7
Kevin Shreve <kshreve@gmail.com>
2019-05-30 08:37:13 -0400

    remove email link

--- js/directives.js
+++ js/directives.js
@@ -1,59 +1,31 @@
-angular.module('designsByReetsie').directive('email', ['$http', function(http) {
-    return {
-        restrict: 'AE',
-        templateUrl: 'emailForm.html',
-        link: function(scope, element, attributes) {
-            scope.disabled = false;
-            
-            element.on('submit', function () {
-                var messageData = {
-                    from: scope.emailForm.emailAddress,
-                    name: scope.emailForm.fname + ' ' + scope.emailForm.lname,
-                    text: scope.emailForm.message,
-                    phone: scope.emailForm.phone
-                };
-                
-                scope.disabled = true;
-                
-                http.post("/sendMail", messageData)
-                    .then(function (data, status, headers, config) {
-                        scope.disabled = false;
-                        
-                        scope.emailForm.emailAddress = '';
-                        scope.emailForm.fname = '';
-                        scope.emailForm.lname = '';
-                        scope.emailForm.message = '';
-                        scope.emailForm.phone = '';
-                    }, function (data, status, headers, config) {
-                        console.log('error', data, status);
-                    });
-            });
-        }
-    };
+angular.module('designsByReetsie').directive('email', [function() {
+  return {
+    restrict: 'AE',
+    templateUrl: 'emailForm.html'
+  };
 }]);
 
-angular.module('designsByReetsie').directive('navBar', [function () {
-    return {
-        restrict: 'A',
-        link: function (scope, element, attributes) {
-            scope.$on('nav-bar-action-toggle', function (event, args) {
-                args.show ? element.addClass('in') : element.removeClass('in');
-            });
-        }
-    };
+angular.module('designsByReetsie').directive('navBar', [function() {
+  return {
+    restrict: 'A',
+    link: function(scope, element) {
+      scope.$on('nav-bar-action-toggle', function(event, args) {
+        args.show ? element.addClass('in') : element.removeClass('in');
+      });
+    }
+  };
 }]);
 
-angular.module('designsByReetsie').directive('navBarAction', [function () {
-    return {
-        restrict: 'A',
-        link: function (scope, element, attributes) {
-            scope.$root.showNav = false;
-            element.on('click', function () {
-                scope.$root.showNav = !scope.$root.showNav;
-                scope.$emit('nav-bar-action-toggle', {show: scope.$root.showNav});
-            });
-            
-        }
-    };
+angular.module('designsByReetsie').directive('navBarAction', [function() {
+  return {
+    restrict: 'A',
+    link: function(scope, element) {
+      scope.$root.showNav = false;
+      element.on('click', function() {
+        scope.$root.showNav = !scope.$root.showNav;
+        scope.$emit('nav-bar-action-toggle', { show: scope.$root.showNav });
+      });
+    }
+  };
 }]);
-    
+
