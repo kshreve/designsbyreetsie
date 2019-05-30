0dd62fd4c28cccd9518f69d30a1c98fee9d5b2a2
Kevin Shreve <kshreve@gmail.com>
2019-05-30 08:51:19 -0400

    reformating code

--- js/app.js
+++ js/app.js
@@ -1,12 +1,32 @@
-angular.module('designsByReetsie', ['ngRoute'])
-    .config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/Home', {templateUrl: 'home.html', name: 'Home', headerName:''})
-            .when('/About', {templateUrl: 'about.html', name: 'About Reetsie', headerName:'About Reetsie'})
-            .when('/Awards', {templateUrl: 'awards.html', controller: 'Award', name: 'Award Winning Gourds', headerName: 'Gourd Art'})
-            .when('/SoldArt', {templateUrl: 'gourdartsold.html', controller: 'GourdArtSold', name: 'Gourd Art Sold', headerName:'Gourd Art Sold'})
-            .when('/Contact', {templateUrl: 'contact.html', controller: 'Contact', name: 'Contact', headerName: 'Contact Reetsie'})
-            .otherwise({redirectTo: '/Home'});
-    }])
-    .constant('Constants', {"ClientId": "Client-Id 4dd8fa5ed1dafe1",
-        "BaseUrl": "https://api.imgur.com/3/album/",
-        "Images": "/images"});
+angular.module('designsByReetsie', ['ngRoute']).config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+    .when('/Home', { templateUrl: 'home.html', name: 'Home', headerName: '' })
+    .when('/About', {
+      templateUrl: 'about.html',
+      name: 'About Reetsie',
+      headerName: 'About Reetsie'
+    })
+    .when('/Awards', {
+      templateUrl: 'awards.html',
+      controller: 'Award',
+      name: 'Award Winning Gourds',
+      headerName: 'Gourd Art'
+    })
+    .when('/SoldArt', {
+      templateUrl: 'gourdartsold.html',
+      controller: 'GourdArtSold',
+      name: 'Gourd Art Sold',
+      headerName: 'Gourd Art Sold'
+    })
+    .when('/Contact', {
+      templateUrl: 'contact.html',
+      controller: 'Contact',
+      name: 'Contact',
+      headerName: 'Contact Reetsie'
+    })
+    .otherwise({ redirectTo: '/Home' });
+}]).constant('Constants', {
+  "ClientId": "Client-Id 4dd8fa5ed1dafe1",
+  "BaseUrl": "https://api.imgur.com/3/album/",
+  "Images": "/images"
+});
--- js/services.js
+++ js/services.js
@@ -1,14 +1,14 @@
-angular.module('designsByReetsie')
-    .factory('ImgurApi', ['$http',  '$route', 'Constants', function (http, route, Constants) {
-        return function () {
-            http.defaults.headers.common['Authorization'] = Constants.ClientId;
-            delete http.defaults.headers.common['X-Requested-With'];
+angular.module('designsByReetsie').factory('ImgurApi', ['$http', '$route', 'Constants',
+  function(http, route, Constants) {
+    return function() {
+      http.defaults.headers.common['Authorization'] = Constants.ClientId;
+      delete http.defaults.headers.common['X-Requested-With'];
 
-            var album = 'ePmjl';
-            if (route.current.$$route.originalPath.replace('/','') == "SoldArt") {
-                album = 'qiBAL';
-            }
+      var album = 'ePmjl';
+      if (route.current.$$route.originalPath.replace('/', '') == "SoldArt") {
+        album = 'qiBAL';
+      }
 
-            return http.get(Constants.BaseUrl.concat(album).concat(Constants.Images));
-        };
-    }]);
+      return http.get(Constants.BaseUrl.concat(album).concat(Constants.Images));
+    };
+  }]);
